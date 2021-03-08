b24bca325c49c05257c97baaf7b97bacedd947fd
Владислав Капустин <70846638+vladhanzo@users.noreply.github.com>
2021-03-08 13:01:57 +0400

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
-
-// You should implement your task here.
-
-module.exports = function towelSort (matrix) {
-  return [];
-}
+module.exports = function towelSort(matrix) {
+    if (!matrix) {
+        return [];
+    }
+    for (let m in matrix) {
+        if (m % 2 !== 0) {
+            matrix[m].reverse();
+        }
+    }
+    let matrix2 = [].concat(...matrix);
+    return matrix2;
+};
